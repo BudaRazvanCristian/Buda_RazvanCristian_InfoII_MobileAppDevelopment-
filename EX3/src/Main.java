1a49048624a771c74bfcf18d2ce9381cf9c5a1f9
budarazvancristian <153955518+BudaRazvanCristian@users.noreply.github.com>
2025-02-18 22:38:06 +0200

    Delete EX3 directory

--- EX3/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(hexToInt("1A3"));
-    }
-
-    public static int hexToInt(String number) {
-        return Integer.parseInt(number, 16);
-    }
-}
